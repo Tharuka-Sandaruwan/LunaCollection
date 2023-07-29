2b4f41f4bdb0a7152df14a277d9339063dff20e6
Savindu Harith <85422289+savinduharith-SDL@users.noreply.github.com>
2023-07-29 12:15:30 +0530

     Cart controller basics

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,26 @@
+import expressAsyncHandler from 'express-async-handler'
+import CartService from '../services/CartService.js'
+
+const addNewItemToCart = expressAsyncHandler(async (req, res) => {
+	const { productID } = req.body
+	const user_id = req.user._id
+	try {
+		const response = await CartService.addItemToUserCart(user_id, productID)
+		res.status(200).json(response)
+	} catch (error) {
+		res.status(500)
+		throw new Error(error)
+	}
+})
+const getCart = expressAsyncHandler(async (req, res) => {
+	try {
+		const user_id = req.user._id
+		const response = await CartService.getUserCart(user_id)
+		res.status(200).json(response)
+	} catch (error) {
+		res.status(500)
+		throw new Error()
+	}
+})
+
+export { addNewItemToCart, getCart }
