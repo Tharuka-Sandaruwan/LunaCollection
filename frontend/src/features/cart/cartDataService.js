a000e0e79268f6bdf5a55b378fe7969e1cf8e00d
Shiran Maduwantha <88098649+shiran2019@users.noreply.github.com>
2023-07-29 14:43:19 +0530

    add cartDataService

---
+++ frontend/src/features/cart/cartDataService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const checkCart = async (id, qty) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+  return {
+    product: data._id,
+    name: data.name,
+    image: data.image,
+    price: data.price,
+    countInStock: data.countInStock,
+    qty: parseInt(qty),
+  };
+};
+
+const cartDataService = {
+  checkCart,
+};
+export default cartDataService;
