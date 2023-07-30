18814f718c2f979fe0d766be75331d951e40e0a8
Savindu Harith <85422289+savinduharith-SDL@users.noreply.github.com>
2023-07-30 07:08:20 +0530

     Cart controller adding and removing items from the cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -12,6 +12,28 @@ const addNewItemToCart = expressAsyncHandler(async (req, res) => {
 		throw new Error(error)
 	}
 })
+const removeItemFromCart = expressAsyncHandler(async (req, res) => {
+	const { productID } = req.body
+	const user_id = req.user._id
+	try {
+		const response = await CartService.dropItemFromCart(user_id, productID)
+		res.status(200).json(response)
+	} catch (error) {
+		throw new Error(error)
+	}
+})
+const addNewCart = expressAsyncHandler(async (req, res) => {
+	try {
+		const user_id = req.user._id
+		// console.log(req.user)
+
+		const response = await CartService.addNewCart(user_id)
+		res.status(200).json(response)
+	} catch (error) {
+		res.status(500)
+		throw new Error(error)
+	}
+})
 const getCart = expressAsyncHandler(async (req, res) => {
 	try {
 		const user_id = req.user._id
@@ -23,4 +45,4 @@ const getCart = expressAsyncHandler(async (req, res) => {
 	}
 })
 
-export { addNewItemToCart, getCart }
+export { addNewCart, addNewItemToCart, getCart, removeItemFromCart }
