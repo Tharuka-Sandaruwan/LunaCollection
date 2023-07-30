6ae80cd4e4113eaf41edfe14765fd3012e2c82e9
Lahiru Ubesinghe <lahiruu-im19053@stu.kln.ac.lk>
2023-07-30 12:19:35 +0530

    Create Footer.js in components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,79 @@
+import { Divider } from 'antd'
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+	return (
+		<footer
+			style={{
+				backgroundColor: 'rgb(52,58,64)',
+			}}
+		>
+			<Container style={{ color: 'white!important' }}>
+				<Row className='justify-content-md-center mx-0 py-3'>
+					<Col>
+						<h2 className=' text-white'>+94-77-100 2158 </h2>
+						<br />
+						<h4 className=' text-white'>
+							109G
+							<br /> Theresa Mawatha,
+							<br /> Kandana
+							<br /> 11320
+						</h4>
+						<br />
+						<a href='mailto:tharindurcg1@gmail.com'>
+							<h6 className=' text-white'>webmaster@example.com </h6>{' '}
+						</a>
+					</Col>
+					<Col>
+						<h2 className=' text-white'>company </h2>
+						<br />
+
+						<a href='#'>
+							<h5 className=' text-white'>about us </h5>{' '}
+						</a>
+						<br />
+						<a href='#'>
+							<h5 className=' text-white'>contact us </h5>{' '}
+						</a>
+						<br />
+						<a href='#'>
+							<h5 className=' text-white'>privacy policy</h5>{' '}
+						</a>
+						<br />
+						<a href='#'>
+							<h5 className=' text-white'>return policy</h5>{' '}
+						</a>
+						<br />
+					</Col>
+					<Col className='text-start '>
+						<h2 className=' text-white'>help </h2>
+						<br />
+
+						<a href='#'>
+							<h5 className=' text-white'>terms n conditions </h5>{' '}
+						</a>
+						<br />
+						<a href='#'>
+							<h5 className=' text-white'>return policy </h5>{' '}
+						</a>
+						<br />
+						<a href='#'>
+							<h5 className=' text-white'>we are hiring</h5>{' '}
+						</a>
+						<br />
+						<div>
+							
+						</div>
+					</Col>
+				</Row>
+				<Divider style={{ backgroundColor: 'white' }} />
+				<Row className='justify-content-md-center mx-0 py-3 text-white'>
+					2023 copyrights@LunaCollectionLK
+				</Row>
+			</Container>
+		</footer>
+	)
+}
+
+export default Footer
