9e9ab52b4e97b60b7bc00e44cf20e7a3ee274701
sandasara <sandasarad@gmail.com>
2023-07-31 11:00:00 +0530

    Completed add product screen

--- frontend/src/screens/AddProductScreen.js
+++ frontend/src/screens/AddProductScreen.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { Form, Button, Row, Col, Image, Card } from 'react-bootstrap'
 import Message from '../components/Message'
+import {
+	addProduct,
+	getProduct,
+	updateProduct,
+} from '../features/products/productAction'
+import { toast } from 'react-toastify'
+import { resetProductStatus } from '../features/products/productDataSlice'
+import axios from 'axios'
 
 function AddProductScreen() {
+	const [productDetails, setProductDetails] = useState({
+		name: '',
+		shortDescription: '',
+		description: '',
+		category: null,
+		price: '',
+		countInStock: 0,
+	})
+	const [categories, setCategories] = useState([])
+	const { id: proID } = useParams()
+
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
+
+	const productReduxState = useSelector((state) => state.productDetails)
+	const logInReduxUserInfo = useSelector((state) => state.logInDetails.userInfo)
+	useEffect(() => {
+		if (logInReduxUserInfo && logInReduxUserInfo.isAdmin) {
+			if (
+				(proID && productReduxState.product._id !== proID) ||
+				(proID && !productDetails._id)
+			) {
+				dispatch(getProduct(proID))
+			}
+			if (categories.length === 0) {
+				axios.get('/api/categories').then(({ data }) => setCategories(data))
+			}
+		} else {
+			navigate('/')
+		}
+	}, [
+		categories.length,
+		dispatch,
+		logInReduxUserInfo,
+		navigate,
+		proID,
+		productDetails._id,
+		productReduxState.product,
+	])
+	if (
+		proID &&
+		productReduxState.action === 'getProduct' &&
+		productReduxState.isSuccess
+	) {
+		dispatch(resetProductStatus())
+		setProductDetails(productReduxState.product)
+	}
+	if (
+		productReduxState.action === 'addProduct' &&
+		productReduxState.isSuccess
+	) {
+		toast.success('Product Successfully added !')
+		dispatch(resetProductStatus())
+		navigate('/admin/productlist')
+	}
+	if (
+		productReduxState.action === 'updateProduct' &&
+		productReduxState.isSuccess
+	) {
+		toast.success('Product Successfully Updated !')
+		dispatch(resetProductStatus())
+		navigate('/admin/productlist')
+	}
+	const changeHandler = (e) => {
+		setProductDetails((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}))
+	}
+	const submitHandler = (e) => {
+		e.preventDefault()
+		if (!proID) {
+			if (productDetails.category) {
+				if (productDetails.countInStock > 0) {
+					dispatch(addProduct(productDetails))
+				} else {
+					toast.error('Count should be greater than 1')
+				}
+			} else {
+				toast.error('Please select category for the product')
+			}
+		} else {
+			if (productDetails.category) {
+				if (typeof productDetails.image === 'string') {
+					dispatch(
+						updateProduct({
+							id: proID,
+							object: { ...productDetails, image: '' },
+						})
+					)
+				} else {
+					console.log(productDetails.image)
+					dispatch(
+						updateProduct({
+							id: proID,
+							object: productDetails,
+						})
+					)
+				}
+			} else {
+				toast.error('Please select category for the product')
+			}
+		}
+	}
 	return (
 		<>
 			<Button
