66a45c8f49f49d3f0cc7ed6b0fae4ea7cc946b41
ts2000614@gmail.com <ts2000614@gmail.com>
2023-07-31 18:05:40 +0530

    removed comments and added status code

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,16 +1,12 @@
-//this errorMiddleware is configured to sends a json object as a response in an error, default error is in an HTML file format
-
-//for urls that aint already defined
 const notFound = (req, res, next) => {
 	const error = new Error(`Not Found -${req.originalUrl}`)
 	res.status(404)
 	next(error)
 }
-//this overrides default error handler for that it needs to be a function that 1st takes in err as a parameter then req,res,next
 const errorHandler = (err, req, res, next) => {
-	//sometimes we get 200 status code even if its an error
-	//500 means server error
+
 	const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+	res.status(statusCode)
 	res.json({
 		message: err.message,
 		stack: process.env.NODE_ENV === 'production' ? null : err.stack,
