1dce4d38344a4ffafb673887711b476c9bbc7765
ts2000614@gmail.com <ts2000614@gmail.com>
2023-07-31 17:14:40 +0530

    eror message improved and added if else

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
 
+const UNAUTHORIZED = 401;
+
 const protect = asyncHandler(async (req, res, next) => {
   try {
     let token;
@@ -10,16 +12,17 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
+      if (!req.user) {
+        throw new Error('User not found');
+      }
       next();
-    }
-
-    if (!token) {
+    } else {
       throw new Error('Not Authorized, no token');
     }
   } catch (error) {
     console.error(error);
-    res.status(401);
-    throw new Error('Not Authorized, token failed');
+    res.status(UNAUTHORIZED);
+    throw new Error('Authorization failed');
   }
 });
 
@@ -27,7 +30,7 @@ const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401);
+    res.status(UNAUTHORIZED);
     throw new Error('Not Authorized as an admin');
   }
 };
