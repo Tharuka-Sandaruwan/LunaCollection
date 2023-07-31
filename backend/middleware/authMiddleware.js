804a05773c701b8187ece432a8a0220382e360c3
ts2000614@gmail.com <ts2000614@gmail.com>
2023-07-31 17:25:40 +0530

    old code restored to fix the error when authorizing

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,38 +1,36 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/userModel.js';
-import asyncHandler from 'express-async-handler';
-
-const UNAUTHORIZED = 401;
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
 
 const protect = asyncHandler(async (req, res, next) => {
-  try {
-    let token;
-
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
-      if (!req.user) {
-        throw new Error('User not found');
-      }
-      next();
-    } else {
-      throw new Error('Not Authorized, no token');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(UNAUTHORIZED);
-    throw new Error('Authorization failed');
-  }
-});
-
+	let token
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		try {
+			token = req.headers.authorization.split(' ')[1]
+			const decoded = jwt.verify(token, process.env.JWT_SECRET)
+			req.user = await User.findById(decoded.id).select('-password')
+			next()
+		} catch (error) {
+			console.error(error)
+			res.status(401)
+			throw new Error('Not Authorized, token failed')
+		}
+	}
+	if (!token) {
+		res.status(401)
+		throw new Error('Not Authorized, no token')
+	}
+	//next()
+})
 const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(UNAUTHORIZED);
-    throw new Error('Not Authorized as an admin');
-  }
-};
-
-export { protect, admin };
+	if (req.user && req.user.isAdmin) {
+		next()
+	} else {
+		res.status(401)
+		throw new Error('Not Authorized as an admin')
+	}
+}
+export { protect, admin }
