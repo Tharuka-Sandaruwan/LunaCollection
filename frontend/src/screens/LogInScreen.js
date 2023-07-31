d146d14074d871b2ec5713dd1a0796ff5003325c
ngnadeera <nadeeragayantha@gmail.com>
2023-07-31 22:25:44 +0530

    added LogIn Form to the Login Screen

--- frontend/src/screens/LogInScreen.js
+++ frontend/src/screens/LogInScreen.js
@@ -43,7 +43,37 @@ function LogInScreen() {
 
 	return (
 		<FormContainer>
-			
+			<h1>Sign In</h1>
+			{isLoading && <Loader />}
+			<Form onSubmit={submitHandler}>
+				<Form.Group controlId='email'>
+					<Form.Label>Email Address</Form.Label>
+					<Form.Control
+						type='email'
+						placeholder='Enter email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group controlId='password'>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type='password'
+						placeholder='Enter password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Button className='my-3' type='submit' variant='primary'>
+					Sign In
+				</Button>
+			</Form>
+			<Row className='py-3'>
+				<Col>
+					New Customer?
+					<Link to={'/register'}>Register</Link>
+				</Col>
+			</Row>
 		</FormContainer>
 	)
 }
