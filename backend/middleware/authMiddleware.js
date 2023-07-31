764a9dbcbbe3fd54a0848a4af41e449264c892e7
ts2000614@gmail.com <ts2000614@gmail.com>
2023-07-31 17:00:40 +0530

    added try catch for exceptions

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,21 +8,19 @@ const protect = asyncHandler(async (req, res, next) => {
 		req.headers.authorization &&
 		req.headers.authorization.startsWith('Bearer')
 	) {
-	
+		try {
 			token = req.headers.authorization.split(' ')[1]
 			const decoded = jwt.verify(token, process.env.JWT_SECRET)
 			req.user = await User.findById(decoded.id).select('-password')
-
+			next()
+		} catch (error) {
 			console.error(error)
 			res.status(401)
 			throw new Error('Not Authorized, token failed')
-		
+		}
 	}
-	if (!token) {
-		res.status(401)
-		throw new Error('Not Authorized, no token')
-	}
-	//next()
+
+	next()
 })
 const admin = (req, res, next) => {
 	if (req.user && req.user.isAdmin) {
