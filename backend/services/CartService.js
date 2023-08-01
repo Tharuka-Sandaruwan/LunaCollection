d8b966361b6b383a00b7b2580d0d7832024d780a
Savindu Harith <85422289+savinduharith-SDL@users.noreply.github.com>
2023-08-01 18:20:18 +0530

     can remove and add new cart functionality added

--- backend/services/CartService.js
+++ backend/services/CartService.js
@@ -32,7 +32,37 @@ const addItemToUserCart = async (userID, productId) => {
 		throw new Error('Failed to update Cart' + error)
 	}
 }
+const dropItemFromCart = async (userID, productID) => {
+	try {
+		const cart = await Cart.findOne({ user: userID }).populate(
+			'cartItems.product'
+		)
+		// console.log(cart)
+		cart.cartItems.pull({ product: productID })
+		// console.log(cart)
+		await cart.save()
+		return await Cart.findOne({ user: userID }).populate('cartItems.product')
+	} catch (error) {
+		throw new Error('Failed to remove item from Cart' + error)
+	}
+}
+const addNewCart = async (userID) => {
+	try {
+		// console.log(userID)
+		const cartExist = await getUserCart(userID)
+		if (cartExist) {
+			throw new Error('Cart for user already exists')
+		} else {
+			const cart = await Cart.create({ user: userID })
+			return cart
+		}
+	} catch (error) {
+		throw new Error(error)
+	}
+}
 const CartService = {
+	addNewCart,
+	dropItemFromCart,
 	addItemToUserCart,
 	getUserCart,
 }
