37c7b093f6be084cf62e7d4e432459e90be56b2d
Savindu Harith <85422289+savinduharith-SDL@users.noreply.github.com>
2023-08-01 20:15:07 +0530

     product controller create,getordered,deleteByID,getById,latestProduct,gettrending product added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,9 @@
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
+import path from 'path'
+import { s3 } from '../server.js'
 import Category from '../models/categoriesModel.js'
+import { AsyncResource } from 'async_hooks'
 
 //@desc Fetch all products
 //@route Get/api/products
@@ -67,7 +70,138 @@ const getLatestProducts = asyncHandler(async (req, res) => {
 	res.json(products)
 })
 
+//@desc fetch tending Products
+//@route Get/api/latestproducts
+//@access public
+const getTrendingProducts = asyncHandler(async (req, res) => {
+	const products = await Product.find()
+		.populate({ path: 'category', selectt: 'name' })
+		.sort({ rating: -1 })
+		.limit(4)
+	res.json(products)
+})
+
+//@desc Fetch all products
+//@route Get/api/productsall
+//@access private/Admin
+const getProductsAll = asyncHandler(async (req, res) => {
+	const products = await Product.find().populate({
+		path: 'category',
+		select: 'name',
+	})
+	res.json(products)
+})
+
+//@desc Fetch single product
+//@route Get/api/products/:id
+//@access public
+const getProductById = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id)
+
+	if (product) {
+		res.json(product)
+	} else {
+		res.status(404)
+		throw new Error('Product not found')
+	}
+})
+//@desc delete product
+//@route Delete/api/products/:id
+//@access private/Admin
+const deleteProductById = asyncHandler(async (req, res) => {
+	const product = await Product.findOne({ _id: req.params.id })
+	const deleteParams = {
+		Key: product.image.split('amazonaws.com/')[1],
+		Bucket: 'luna-product-images',
+	}
+	s3.deleteObject(deleteParams, (err, data) => {
+		if (err) {
+			throw new Error('Failed to delete Image')
+		}
+		if (data) {
+			product.delete().then(
+				(data) => res.json('Product Successfully deleted'),
+				(err) => {
+					throw new Error(
+						'Failed to remove item from DB, contact system Administrator'
+					)
+				}
+			)
+		}
+	})
+})
+
+//@desc create product
+//@route PUT/api/products/:id
+//@access private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+	const newProduct = new Product({
+		user: req.user._id,
+		...req.body,
+	})
+	const Key = `${newProduct._id}-image${path.extname(req.file.originalname)}`
+	const uploadParams = {
+		Key: Key,
+		Bucket: 'luna-product-images',
+		Body: req.file.buffer,
+	}
+	s3.putObject(uploadParams, async (err, data) => {
+		if (err) {
+			res.status(400)
+			throw new Error(`Failed to upload file to Server, please retry` + err)
+		}
+		if (data) {
+			newProduct[
+				'image'
+			] = `https://luna-product-images.s3.ap-south-1.amazonaws.com/${Key}`
+			await newProduct.save().then(
+				(data) => res.json(`Product Successfully added`),
+				(error) => {
+					let deleteParams = {
+						Key: Key,
+						Bucket: 'luna-product-images',
+					}
+					s3.deleteObject(deleteParams, (err, data) => {
+						if (err) {
+							res.status(500)
+							throw new Error(
+								'Failed to add product to System and also failed delete the uploaded file from Bucket:' +
+									err
+							)
+						}
+						if (data) {
+							res.status(500)
+							throw new Error(`Failed to add product to the system` + error)
+						}
+					})
+				}
+			)
+		}
+	})
+})
+//@desc Get all the products ordered by user
+//@route GET/api/products/ordered/
+//@access Private
+const getProductsOrdered = asyncHandler(async (req, res) => {
+	const productIds = await Order.distinct('orderItems.product', {
+		user: req.user._id,
+		completed: true,
+	})
+	if (productIds) {
+		res.json(productIds)
+	} else {
+		res.status(401)
+		throw new Error('products not found')
+	}
+})
 
 export {
-	getProducts
+	getProducts,
+	getLatestProducts,
+	getProductById,
+	deleteProductById,
+	createProduct,
+	getTrendingProducts,
+	getProductsAll,
+	getProductsOrdered,
 }
