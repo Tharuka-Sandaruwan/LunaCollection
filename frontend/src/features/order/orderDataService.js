0c61a656d88e355b93ec7e2aaa44544336b9a59d
Shiran Maduwantha <88098649+shiran2019@users.noreply.github.com>
2023-08-01 23:21:20 +0530

    add more order functions and update the code in orderDataService

--- frontend/src/features/order/orderDataService.js
+++ frontend/src/features/order/orderDataService.js
@@ -48,11 +48,46 @@ const getAllOrders = async () => {
   const { data } = await axios.get(`/api/orders/`, config);
   return data;
 };
-
+const setOrderShipped = async (id) => {
+  const token = decodeURI(getCookie("token"));
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data } = await axios.get(`/api/orders/ship/${id}`, config);
+  return data;
+};
+const setOrderDelivered = async (id) => {
+  const token = decodeURI(getCookie("token"));
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data } = await axios.get(`/api/orders/deliver/${id}`, config);
+  return data;
+};
+const getUserOrders = async (id) => {
+  const token = decodeURI(getCookie("token"));
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const { data } = await axios.get(`/api/orders/user/${id}`, config);
+  return data;
+};
 const orderDataService = {
   getOrderById,
   makeOrder,
   getMyOrders,
   getAllOrders,
+  setOrderDelivered,
+  setOrderShipped,
+  getUserOrders,
 };
 export default orderDataService;
