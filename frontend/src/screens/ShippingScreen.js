251c1d954b85fd942507317e4de1acf986e3795f
Anupazen <86473829+Anupazen@users.noreply.github.com>
2023-08-02 09:16:33 +0530

    Ship screen 1

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+import { Button, Form } from 'react-bootstrap'
+import { addShippingAddress } from '../features/cart/cartDataSlice'
+import { useNavigate } from 'react-router-dom'
+const ShippingScreen = () => {
+	const shippingAddress = useSelector((state) => state.cart.shippingAddress)
+	const [formFields, setFormFields] = useState({
+		address: shippingAddress.address,
+		city: shippingAddress.city,
+		postalCode: shippingAddress.postalCode,
+	})
+	const { address, city, postalCode } = formFields
+	const dispatch = useDispatch()
+
+	const navigate = useNavigate()
+	const submitHandler = (e) => {
+		e.preventDefault()
+		dispatch(addShippingAddress(formFields))
+		navigate('/placeorder')
+	}
+	const handleOnChange = (e) => {
+		setFormFields((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}))
+	}
+	return (
+		<>
+			<FormContainer>
+				<h1>Shipping</h1>
+				<Form onSubmit={submitHandler}>
+					<Form.Group controlId='address'>
+						<Form.Label>Address</Form.Label>
+						<Form.Control
+							type='text'
+							name='address'
+							placeholder='Enter address'
+							value={address}
+							required
+							onChange={handleOnChange}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId='city'>
+						<Form.Label>City</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Enter city'
+							value={city}
+							name='city'
+							required
+							onChange={handleOnChange}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId='postalCode'>
+						<Form.Label>Postal Code</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Enter postal code'
+							value={postalCode}
+							required
+							name='postalCode'
+							onChange={handleOnChange}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId='country'>
+					<Form.Label>Country</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter country'
+						value={country}
+						name='country'
+						required
+						onChange={handleOnChange}
+					></Form.Control>
+				</Form.Group>
+					<Button type='submit' variant='primary'>
+						Continue
+					</Button>
+				</Form>
+			</FormContainer>
+		</>
+	)
+}
+
+export default ShippingScreen
