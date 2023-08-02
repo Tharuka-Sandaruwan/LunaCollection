beb2691be24839816826804f0f89cc31e3850e43
tnimesh12345 <88110398+tnimesh12345@users.noreply.github.com>
2023-08-02 11:43:22 +0530

    after create add product and get product functions

---
+++ frontend/src/features/products/productAction.js
@@ -0,0 +1,103 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import productService from "./productService";
+
+//getProducts
+export const listProducts = createAsyncThunk(
+  "products/getAll",
+  async (object, thunkAPI) => {
+    try {
+      return await productService.getProducts(object);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+//getLatestProducts
+export const latestProducts = createAsyncThunk(
+  "products/getLatest",
+  async (_, thunkAPI) => {
+    try {
+      return await productService.getLatestProducts();
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+//getTrendingProducts
+export const trendingProducts = createAsyncThunk(
+  "products/getTrending",
+  async (_, thunkAPI) => {
+    try {
+      return await productService.getTrendingProducts();
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+//getAllProducts
+export const getAllProducts = createAsyncThunk(
+  "products/getAllProducts",
+  async (_, thunkAPI) => {
+    try {
+      return await productService.getProductsAll();
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+export const getProduct = createAsyncThunk(
+  "products/get",
+  async (id, thunkAPI) => {
+    try {
+      return await productService.listProduct(id);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+export const addProduct = createAsyncThunk(
+  "product/add",
+  async (object, thunkAPI) => {
+    try {
+      return await productService.addProduct(object);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
