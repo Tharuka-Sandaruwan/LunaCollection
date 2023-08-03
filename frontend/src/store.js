4d7aa7defd428a84d40bfbf48b5b8f430168e305
ngnadeera <nadeeragayantha@gmail.com>
2023-08-03 22:44:14 +0530

    Implement productDataReducer for managing product details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import productListDataReducer from './features/products/productListDataSlice'
+import productDataReducer from './features/products/productDataSlice'
 
 
 const store = configureStore({
 	reducer: {
 		productList: productListDataReducer,
+		productDetails: productDataReducer,
 		
 	},
 })
