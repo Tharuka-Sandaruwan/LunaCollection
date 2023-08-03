049cf94715982d93efc4ab1465d2e64a070bbe00
ngnadeera <nadeeragayantha@gmail.com>
2023-08-03 12:12:34 +0530

    added productListDataReducer to handle product list data

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import productListDataReducer from './features/products/productListDataSlice'
 
 
 const store = configureStore({
-	
+	reducer: {
+		productList: productListDataReducer,
+		
+	},
 })
 
 export default store
