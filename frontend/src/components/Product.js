40e41549328986ab6e2b43f50dfef690d900add1
Lahiru Ubesinghe <lahiruu-im19053@stu.kln.ac.lk>
2023-08-04 12:50:03 +0530

    Create:Finished Product  in  components

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,6 +5,26 @@ import { Link } from 'react-router-dom'
 
 const Product = ({ product }) => {
 	return (
+    <Card className='my-3 p-3 '>
+			<Link to={`/product/${product._id}`}>
+				<Card.Img src={product.image} variant='top' />
+			</Link>
+			<Card.Body>
+				<Link to={`/product/${product._id}`}>
+					<Card.Title as='div'>
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</Link>
+				<Card.Text as='div'>
+					<Rating
+						value={product.rating}
+						text={`${product.numReviews} reviews`}
+						//color='red'
+					/>
+				</Card.Text>
+				<Card.Text as='h3'>Rs.{product.price}</Card.Text>
+			</Card.Body>
+		</Card>
 
 	)
 }
