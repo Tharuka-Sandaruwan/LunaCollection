ad6eb0bf54199fd8d903119bf4db7b5fda28e7bd
ngnadeera <nadeeragayantha@gmail.com>
2023-08-04 08:10:22 +0530

    Create several DataReducers to handle shopping cart and order related data

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,20 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import productListDataReducer from './features/products/productListDataSlice'
 import productDataReducer from './features/products/productDataSlice'
+import cartDataReducer from './features/cart/cartDataSlice'
+import orderDataReducer from './features/order/orderDataSlice'
+import logInDataReducer from './features/users/logInDataSlice'
+import profileDataReducer from './features/users/profileDataSlice'
 
 
 const store = configureStore({
 	reducer: {
 		productList: productListDataReducer,
 		productDetails: productDataReducer,
+		cart: cartDataReducer,
+		logInDetails: logInDataReducer,
+		profileDetails: profileDataReducer,
+		orders: orderDataReducer,
 		
 	},
 })
