4f0d241fd9bcc11c1c4875914a41f9c1ff6ede34
tnimesh12345 <88110398+tnimesh12345@users.noreply.github.com>
2023-08-04 23:56:16 +0530

    after create getprofile and update profile

--- frontend/src/features/users/UserActions.js
+++ frontend/src/features/users/UserActions.js
@@ -36,3 +36,56 @@ export const registerUser = createAsyncThunk(
     }
   }
 );
+
+export const getProfile = createAsyncThunk(
+  "users/getProfile",
+  async (_, thunkAPI) => {
+    try {
+      const data = await userService.getProfile();
+      return data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+export const getAllUsers = createAsyncThunk(
+  "users/getAllUsers",
+  async (_, thunkAPI) => {
+    try {
+      const data = await userService.getAllUsers();
+      return data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+export const updateProfile = createAsyncThunk(
+  "users/updateProfile",
+  async ({ name, email, password }, thunkAPI) => {
+    try {
+      const data = await userService.updateProfile({ name, email, password });
+      return data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
