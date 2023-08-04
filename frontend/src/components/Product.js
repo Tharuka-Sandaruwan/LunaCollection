9bd6d02df0d0c90ce375bc8301e7705ec5d2f27f
Lahiru Ubesinghe <lahiruu-im19053@stu.kln.ac.lk>
2023-08-04 07:30:53 +0530

    Create:not completed Product  in  components

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import { Link } from 'react-router-dom'
+
+const Product = ({ product }) => {
+	return (
+
+	)
+}
+
+export default Product
