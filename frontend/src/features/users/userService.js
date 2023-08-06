eef28cc50c6a6ebb6c3c9c88554a47eaf12aab18
tnimesh12345 <88110398+tnimesh12345@users.noreply.github.com>
2023-08-06 08:41:44 +0530

    after create service function to getprofile and getallusers

--- frontend/src/features/users/userService.js
+++ frontend/src/features/users/userService.js
@@ -14,3 +14,26 @@ const register = async ({ name, email, password }) => {
   const response = await axios.post("/api/users/", { name, email, password });
   return response.data;
 };
+
+const getProfile = async () => {
+  const token = decodeURI(getCookie("token"));
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.get(`/api/users/profile`, config);
+  return response.data;
+};
+const getAllUsers = async () => {
+  const token = decodeURI(getCookie("token"));
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.get(`/api/users/`, config);
+  return response.data;
+};
